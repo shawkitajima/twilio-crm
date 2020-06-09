b427eb247c19c44160df3e9e24c56175c47aef34
Shaw Kitajima <shawkitajima@gmail.com>
2020-06-09 13:25:27 -0700

    sets up login and signup backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/users');
 
-router.get('/', usersCtrl.sayHello);
-
+router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
 
 module.exports = router;
