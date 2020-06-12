2a7409dbea6a9ca614af9a7dc7ad110c8731f88c
Shaw Kitajima <shawkitajima@gmail.com>
2020-06-12 12:58:10 -0700

    when a user adds a new field, each of that user's contacts fields gets that field with a null value

--- controllers/fields.js
+++ controllers/fields.js
@@ -1,11 +1,32 @@
-const Field = require('../models/field')
+const Field = require('../models/field');
+const User = require('../models/user');
+const Contact = require('../models/contact');
 
 module.exports = {
     create,
 }   
 
 function create(req, res) {
-    Field.create(req.body, function(err, field) {
-        res.send(field);
-    });
+    User.findById(req.body.id, function(err, user) {
+        Field.create(req.body.field, function(err, field) {
+            if (err) {
+                console.log(err);
+                return res.send({errors: err});
+            }
+            let contactFields = [...user.contactFields];
+            contactFields.push(field._id);
+            res.send({errors: 'none'});
+            User.findByIdAndUpdate(user._id, {contactFields}, function(err, updatedUser) {
+                Contact.find({owner: user._id}, function(err, contacts) {
+                    contacts.forEach(contact => {
+                        let fields = {...contact.fields};
+                        fields[field.name] = null;
+                        Contact.findByIdAndUpdate(contact._id, {fields}, function(err, updatedContact) {
+                            if (err) console.log(err);
+                        });
+                    });
+                });
+            });
+        });
+    })
 }
